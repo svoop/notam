3d1b926eb2bae48dd37bf7a5a4a4b28c3000b29e
Sven Schwyn <schwyn@bitcetera.com>
2022-03-29 21:47:45 +0200

    Add rake tasks to download fixtures for tests

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,7 @@
-require 'bundler/gem_tasks'
+require 'fileutils'
+require 'net/http'
 
+require 'bundler/gem_tasks'
 require 'rake/testtask'
 
 Rake::TestTask.new do |t|
@@ -18,6 +20,35 @@ namespace :yard do
       `open http://localhost:8808`
     end
     `yard server -r`
+  end
+end
+
+namespace :fixtures do
+  desc "Fetch new test NOTAM fixtures for comma separated ICAO codes from DINS "
+  task :fetch, [:icao_codes] do |_, args|
+    icao_codes = (args[:icao_codes] || 'LFXX,EDXX,LSAS').gsub(/\W+/, ' ')
+    fixtures_path.mkdir unless fixtures_path.exist?
+    response = Net::HTTP.post_form(
+      URI('https://www.notams.faa.gov/dinsQueryWeb/queryRetrievalMapAction.do'),
+      actionType: 'notamRetrievalByICAOs',
+      reportType: 'Raw',
+      retrieveLocId: icao_codes
+    ).tap do |response|
+      fail "bad response from DINS" unless response.code == '200'
+      response.body.scan(/<pre>(.+?)<\/pre>/im) do |message|
+        id = message.first.split(/\s/, 2).first.sub(/\W+/, '_')
+        File.write(fixtures_path.join("#{id}.txt"), message.first)
+      end
+    end
+  end
+
+  desc "Clean current test NOTAM fixtures"
+  task :clean do
+    fixtures_path.rmtree if fixtures_path.exist?
+  end
+
+  def fixtures_path
+    Pathname(Dir.pwd).join('spec', 'fixtures')
   end
 end
 
